08e52addd1e235a26bac71cc37edfee65c676396
Hisano Sato <hisano@Hisanos-MacBook-Air.local>
2023-07-09 12:04:30 +1000

    add project page

---
+++ src/Pages/Project.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Styles/Project.css";
+
+function Project() {
+  return (
+    <div>
+      <h1>coming soon...</h1>
+    </div>
+  );
+}
+
+export default Project;
